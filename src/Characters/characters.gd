28f0213efb0c890d40b61f58aa61481c78347469
Ladzsz <londonmason602@gail.com>
2026-02-11 19:07:21 -0500

    added frame for basic level and player movement

--- src/Characters/characters.gd
+++ src/Characters/characters.gd
@@ -2,6 +2,13 @@ extends CharacterBody2D
 class_name Characters
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2(300, 0)
+	
+	var baseSpeed = Vector2(300, 1000)
+	var baseGravity = 980
+	
+	velocity.x = Input.get_axis("ui_left", "ui_right") * baseSpeed.x
+	
+	velocity.y += baseGravity * delta
+	
 	move_and_slide()
 	
--- src/Characters/hero.gd
+++ src/Characters/hero.gd
@@ -1,1 +1,11 @@
 extends "res://src/Characters/characters.gd"
+
+
+func _physics_process(delta: float) -> void:
+	const JUMP_VELOCITY = -400.0
+	
+	# Handle Jump if on floor.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	super._physics_process(delta)	
