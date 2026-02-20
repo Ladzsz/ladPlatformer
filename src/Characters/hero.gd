eca2fef292e03891a15f3a588aa6b72b9c8225ba
Ladzsz <londonmason602@gmail.com>
2026-02-20 18:44:36 -0500

    added new asset pack

--- src/Characters/hero.gd
+++ src/Characters/hero.gd
@@ -1,5 +1,7 @@
 extends Characters
 
+var is_ducking = false
+
 #overiding die from characters
 func die():
 	print(get_tree().get_nodes_in_group("death_screen"))
@@ -12,13 +14,44 @@ func die():
 
 #user input
 func getInput():
+	
 	const JUMP_VELOCITY = -400.0
 	
 	velocity.x = Input.get_axis("ui_left", "ui_right") * baseSpeed.x
 	
 	# Handle Jump if on floor.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		$AnimatedSprite2D.play("jump")
 		velocity.y = JUMP_VELOCITY
+	elif not is_on_floor():
+		$AnimatedSprite2D.play("jump")
+		
+	#walk animation
+	if not is_ducking:
+		if abs(velocity.x) > 0:
+			$AnimatedSprite2D.play("walk")
+		else:
+			$AnimatedSprite2D.play("idle")
+
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+	elif velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+		
+	#duck animation
+	if Input.is_action_pressed("ui_down") and is_on_floor():
+		if not is_ducking:
+			is_ducking = true
+			$AnimatedSprite2D.play("duck")
+			$AnimationPlayer.play("duck")
+	else:
+		if is_ducking:
+			is_ducking = false
+			$AnimationPlayer.play_backwards("duck")
+			$AnimatedSprite2D.play("idle")
+		
+	
+		
 	
 #checking if enemies colided with player body
 func _on_area_2d_body_entered(body: Node2D) -> void:
